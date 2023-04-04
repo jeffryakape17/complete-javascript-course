6137dbc9408a2428df4f6d774011dc52a2a77048
jeffryakape17 <nevershownever@gmail.com>
2023-04-04 21:08:59 +0700

    Complete video no.20

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,51 +1,46 @@
-/*const markMass = 95;
-const johnMass = 85;
-const markHeight = 1.88;
-const johnHeight = 1.76;
+// const markMass = 95;
+// const johnMass = 85;
+// const markHeight = 1.88;
+// const johnHeight = 1.76;
+// const markBmi = markMass / (markHeight ** 2);
+// const johnBmi = johnMass / (johnHeight ** 2);
 
-const markHigherBMI = markMass / (markHeight ** 2) > johnMass / (johnHeight ** 2);
-console.log('mark higher BMI? ' + markHigherBMI);
-*/
+// const markHigherBMI = markMass / (markHeight ** 2) > johnMass / (johnHeight ** 2);
+// console.log('mark higher BMI? ' + markHigherBMI);
 
-/*
-const country = 'Indonesia';
-const continent = 'Asia';
-let population = '281';
-console.log(country);
-console.log(continent);
-console.log(population);
+// if (markBmi > johnBmi) {
+//     console.log(`Mark's BMI (${markBmi}) is higher than John's (${johnBmi})`)
+// } else {
+//     console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi})`)
+// }
 
-const isIsland = true;
-const language = 'Bahasa'
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
-console.log(population / 2);
-console.log(population + 1);
-console.log(population > 6);
-console.log(population < 33);
-const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
-const descriptionTemplate = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-console.log(description);
-console.log(descriptionTemplate);
-*/
 
-const age = 15;
-const isOldEnough = age >= 18;
+// const country = 'Indonesia';
+// const continent = 'Asia';
+// let population = '281';
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-if (isOldEnough === true) {
-    console.log('Sarah can start driving license 😊');
-} else {
-    console.log('Sarah cannot start driving license 😣');
-}
+// const isIsland = true;
+// const language = 'Bahasa'
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+// console.log(population / 2);
+// console.log(population + 1);
+// console.log(population > 6);
+// console.log(population < 33);
+// const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+// const descriptionTemplate = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(description);
+// console.log(descriptionTemplate);
 
-const birthYear = 1986;
-let century;
+// if (population > 33) {
+//     console.log(`${country}'s population is above average`);
+// } else {
+//     let belowAverage = 33 - population;
+//     console.log(`${country}'s population is ${belowAverage} million below average`);
+// }
 
-if (birthYear == 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
-console.log(century);
